a251aecedf78a6a5a02b27ceddf38903ff109a30
clockwork189 <charles.desouza3000@gmail.com>
2012-09-02 15:20:43 -0400

    Calculator works

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
 
 @interface CalculatorViewController()
 @property (nonatomic) BOOL userInTheMiddleOfEnteringNumber; 
+@property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
 
@@ -18,6 +20,12 @@
 
 @synthesize display;
 @synthesize userInTheMiddleOfEnteringNumber;
+@synthesize brain = _brain;
+
+-(CalculatorBrain *) brain {
+    if(!_brain) _brain = [[CalculatorBrain alloc] init];
+    return _brain;
+}
 
 - (IBAction)digitPressed:(UIButton *)sender {
     if(self.userInTheMiddleOfEnteringNumber) {
@@ -30,9 +38,16 @@
 }
 
 - (IBAction)enterPressed {
+    [self.brain pushOperand:[self.display.text doubleValue]];
+    self.userInTheMiddleOfEnteringNumber = NO;
 }
 
 - (IBAction)operationPressed:(UIButton *)sender {
+    if(self.userInTheMiddleOfEnteringNumber) {
+        [self enterPressed];
+    }
+    double result = [self.brain performOperation:sender.currentTitle];
+    self.display.text = [NSString stringWithFormat:@"%g", result];
 }
 
 @end
